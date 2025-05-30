d491aa05fc9dcf926ef2ef0911d553054531f720
Fernando <fernando@email.com>
2025-05-30 19:56:02 -0300

    Codigo spotifom.c comentado

--- codigos/spotyfom.c
+++ codigos/spotyfom.c
@@ -122,6 +122,9 @@ void ler_backup(struct desc *acervo, char nome[]){
 }
 
 void execucao(struct musica *mus){
+
+	// Recebe o ponteiro da struct musica e incrementa sua execucao em 1
+	// por ser o endereco muda o valor em todo lugar em que a struct foi passada
 	(mus->execucoes++);
 }
 
@@ -129,12 +132,15 @@ void cria_playlist_aleatoria(struct desc *acervo, struct desc_queue *aleatoria){
 	int num = 0, y=0;
 	struct nodo *aux;
 
+	// seta o descritor da fila como null por precaucao
 	aleatoria->head = NULL;
 	aleatoria->tail = NULL;
 	aleatoria->tamanho = 0;
 
+	// gerar uma seed aleatoria baseada no tempo de execucao para escolha aleatória
 	srand(time(NULL));
 
+	// recebe do usuario quantas musicas ele deseja na playlist
 	printf("\nDigite numero de musicas na playlist: ");
 	setbuf(stdin,NULL);
 	scanf("%i",&num);
@@ -142,16 +148,25 @@ void cria_playlist_aleatoria(struct desc *acervo, struct desc_queue *aleatoria){
 
 	if(num > 0){
 		for(int x=0;x<num;x++){
+			// gera valor aleatorio para posicao a ser buscada no acervo
 			y = (rand() % acervo->tamanho);
+
+			// coloca aux no inicio do acervo
 			aux = acervo->lista;
+
+			// busca posicao aleatoria
 			while(y>0){
 				aux=aux->prox;
 				y--;
 			}
+
+			// enfilera a musica buscada na playlist
 			ENQUEUE(aleatoria,aux->info);
 		}
 
 		printf("Playlist aleatoria criada com sucesso!!!\nconteudo:\n\n");
+
+		//apresenta ao usuario a sua plylist
 		ShowQueue(aleatoria);
 
 	}
@@ -162,7 +177,10 @@ void executa_aleatoria(struct desc_queue *ale){
 
 	struct nodo_queue *aux;
 
+	// executa playlist ate nao sobrar nada
 	while(ale->head != NULL){
+
+		// desenfilera fila e salva conteudo temporariamente em aux para execurar e apresentar ao usuario
 		aux = DEQUEUE(ale);
 		execucao(aux->info);
 		printf("\n");
@@ -171,18 +189,23 @@ void executa_aleatoria(struct desc_queue *ale){
 
 	printf("\nPlaylist executada com sucesso!!!\n");
 
+	// a operacao anterior ja deixa a cabeca e cauda em NULL e tamanho em 0
+	// feito por precaucao
 	ale->head = NULL;
 	ale->tail = NULL;
 	ale->tamanho = 0;
 }
 
 void cria_playlist_usuario(struct desc *acervo, struct desc_pilha *user){
+
+	// criacao das variaveis da funcao
 	user->topo = NULL;
 	user->tamanho = 0;
 	int x = 0, y = 0;
 	char temp[255];
 	struct nodo *aux;
 
+	// menu para escolher forma de insercao
 	printf("\n\n==========BUSCA==========\n1.Nome\n2.Codigo\n Digite opcao: ");
 	setbuf(stdin,NULL);
 	scanf("%i",&x);
@@ -191,15 +214,19 @@ void cria_playlist_usuario(struct desc *acervo, struct desc_pilha *user){
 		case 1:
 			while(x != 0){
 
+				// recebe nome da musica a ser buscada
 				printf("\nDigite nome da musica: ");
 				setbuf(stdin,NULL);
 				scanf("%[^\n]s",temp);
 				temp[strcspn(temp, "\n")] = '\0';
 
+				// seta variaveis a serem utilizadas para o valor desejado
 				y = 0;
 				aux = acervo->lista;
 
 				while(y == 0 && aux !=NULL){
+
+					// Busca a musica desejada no acervo e empilha na playlist
 					if(strcasecmp(temp,aux->info->titulo) == 0){
 						y = 1;
 						PUSH(user, aux->info);
@@ -210,22 +237,29 @@ void cria_playlist_usuario(struct desc *acervo, struct desc_pilha *user){
 					aux = aux->prox;
 				}
 
+				// Informa se a musica nao foi encontrada
 				if(y = 0) printf("\nEsta musica nao esta no acervo\n");
 
+				// pergunta de o usuario deseja por mais musicas na playlist
 				printf("\nDeseja adicionar mais musicas(sim=1 nao=0)? ");
 				setbuf(stdin,NULL);
 				scanf("%i",&x);
 			}
 
+			// apresenta a playlist criada
 			if(user->topo != NULL){
 				printf("\n\nMusicas da playlist criada: \n\n");
 				imprime_pilha(user);
 			}
+
+			// informa se nada foi feito
 			else printf("\n\nNenhuma musica foi adicionada!!!\n");
 
 			break;
 		case 2:
 			while(x != 0){
+
+				// o mesmo que foi feito com o nome, mas agora com o codigo da musica
 				printf("\nDigite codigo da musica: ");
 					setbuf(stdin,NULL);
 					scanf("%i",&x);
@@ -268,6 +302,8 @@ void executa_usuario(struct desc_pilha *user){
 
 	struct nodo_pilha *aux;
 
+	// esvazia a playlist com POP salvando temporariamente seu conteudo para executar a musica ate chegar a NULL
+	// no final de cada loop mostra na tela a musica tocada
 	while(user->topo != NULL){
 		aux = POP(user);
 		execucao(aux->info);
@@ -291,14 +327,19 @@ void relatorio_acervo(struct desc *acervo, char nome[]){
 		return;
 	}
 
-	struct nodo *aux = acervo->lista;
+	struct nodo *aux = acervo->lista; //auxiliar para percorrer acervo
 
+	// Informa quantas musicas existem no acervo
 	fprintf(arq,"Tamanho do acervo = %i\n\nMusicas:\n",acervo->tamanho);
 
 	while(aux != NULL){
+
+		// escreve as informacoes da musica no arquivo
 		fprintf(arq,"\nTitulo: %s\nArtista: %s\nLetra: %s\nCodigo: %i\nExecucoes: %i\n", aux->info->titulo, aux->info->artista, aux->info->letra, aux->info->codigo, aux->info->execucoes);
 		aux = aux->prox;
 	}
+
+	printf("\nRelatorio criado com sucesso!!!\n");
 }
 
 void relatorio_playlists(struct desc_pilha *user, struct desc_queue *ale, char nome[]){
@@ -309,13 +350,16 @@ void relatorio_playlists(struct desc_pilha *user, struct desc_queue *ale, char n
 		return;
 	}
 
+	//auxiliares para percorrer playlists
 	struct nodo_pilha *aux_user = user->topo;
 	struct nodo_queue *aux_ale = ale->head;
 
-	if(user->topo != NULL){
+	if(user->topo != NULL){ //informa a playlist que sera escrita e se existem elementos
 		fprintf(arq,"Playlist do usuario:\n");
 
 		while(aux_user != NULL){
+
+			//escreve informacoes das musicas
 			fprintf(arq,"\nTitulo: %s\nArtista: %s\nLetra: %s\nCodigo: %i\nExecucoes: %i\n", aux_user->info->titulo, aux_user->info->artista, aux_user->info->letra, aux_user->info->codigo, aux_user->info->execucoes);
 			aux_user = aux_user->prox;
 		}
@@ -323,6 +367,7 @@ void relatorio_playlists(struct desc_pilha *user, struct desc_queue *ale, char n
 	else fprintf(arq,"Playlist do usuario vazia\n\n");
 
 
+	//mesma logica do bloco anterior
 	if(ale->head != NULL){
 		fprintf(arq,"\n\nPlaylist aleatoria:\n");
 
@@ -332,4 +377,6 @@ void relatorio_playlists(struct desc_pilha *user, struct desc_queue *ale, char n
 		}
 	}
 	else fprintf(arq,"\n\nPlaylist aleatoria vazia\n\n");
+
+	printf("\nRelatorio criado com sucesso!!!\n");
 }
